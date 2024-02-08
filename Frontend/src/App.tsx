a7fba188296803f98c35c60a8648faa090b6170a
j._.jinoo <jinoo0306@naver.com>
2024-02-09 04:15:23 +0900

    chore : vite+ts 프론트엔드 개발 세팅 완료

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import GlobalStyle from "./GlobalStyles";
 import MainPage from "../pages/MainPage";
 
 function App() {
   return (
     <>
-      <GlobalStyle />
       <Routes>
         <Route path="/" element={<MainPage />} />
       </Routes>
