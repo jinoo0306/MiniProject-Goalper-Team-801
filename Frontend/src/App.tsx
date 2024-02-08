752ae987cefc0ecb392633b5c11a0fe53b7af819
j._.jinoo <jinoo0306@naver.com>
2024-02-09 02:48:01 +0900

    chore : react+ts 초기 세팅

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import GlobalStyle from "./GlobalStyles";
 import MainPage from "../pages/MainPage";
 
@@ -6,8 +7,9 @@ function App() {
   return (
     <>
       <GlobalStyle />
-
-      <Route path="/" element={<MainPage />} />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
     </>
   );
 }
