a61e66831ce93316881ddbc85a1ee32fa2f5a3e5
j._.jinoo <jinoo0306@naver.com>
2024-02-16 06:42:41 +0900

    design : tailwind css 적용 및 Status 컴포넌트 디자인 완료

---
+++ Frontend/src/components/public/status.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function MainPage() {
+  const statusCode = 0;
+  const statusBackgroundColor = [
+    "bg-gray-300",
+    "bg-yellow-100",
+    "bg-green-200",
+  ];
+  const statusFontColor = [
+    "text-gray-600",
+    "text-yellow-700",
+    "text-green-700",
+  ];
+  const statusText = ["Not Started", "In Progress", "Completed"];
+  return (
+    <div
+      className={`flex justify-center items-center w-32 h-12 rounded-full ${statusBackgroundColor[statusCode]} ${statusFontColor[statusCode]} text-base`}
+    >
+      {statusText[statusCode]}
+    </div>
+  );
+}
+
+export default MainPage;
