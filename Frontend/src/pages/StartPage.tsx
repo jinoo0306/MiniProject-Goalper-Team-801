9a8061997b84f4cc367670a40ce0edfaf2fc6d9e
j._.jinoo <jinoo0306@naver.com>
2024-02-18 05:54:46 +0900

    design : Start Page 퍼블리싱 완료

--- Frontend/src/pages/StartPage.tsx
+++ Frontend/src/pages/StartPage.tsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import ToDoListImage from "../assets/images/StartPage/ToDoList.png";
 import BulletinBoard from "../assets/images/StartPage/BulletinBoard.png";
 import BuyBooks from "../assets/images/StartPage/BuyBooks.png";
 
 function StartPage() {
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate("/login");
+  };
+
   return (
     <div className="flex flex-col justify-center items-center">
       <h1 className="text-8xl font-bold mt-32">The best helper for studying</h1>
-      <h2 className="text-4xl font-bold mt-11 text-gray-500">
+      <h2 className="text-4xl font-bold mt-14 text-gray-500">
         Get the best help for your studies with 801 study
       </h2>
       <div className="flex flex-row justify-center items-center mt-28 w-100 h-96">
@@ -23,6 +30,12 @@ function StartPage() {
           <img src={BuyBooks} alt="To Do List Image" className="h-64" />
           <a className="text-5xl mt-4 font-bmjua">Buy Books</a>
         </div>
+      </div>
+      <div
+        className="flex flex-col justify-center items-center mt-28 w-72 h-28 bg-black rounded-full text-white text-3xl cursor-pointer hover-scale"
+        onClick={handleButtonClick}
+      >
+        Get started
       </div>
     </div>
   );
