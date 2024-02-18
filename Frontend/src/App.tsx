bc5ed490f79cf88728a57108b57b015d0cf365cb
j._.jinoo <jinoo0306@naver.com>
2024-02-19 03:25:53 +0900

    design : Passion 폰트 추가

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,12 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import StartPage from "../src/pages/StartPage";
 import MainPage from "../src/pages/MainPage";
 import LoginPage from "../src/pages/LoginPage";
+import NavigationBar from "../src/components/public/NavigationBar";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<StartPage />} />
+        <Route path="/" element={<NavigationBar />} />
         <Route path="/main" element={<MainPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
---
+++ Frontend/src/components/public/NavigationBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NavigationBar() {
+  return (
+    <div className="flex flex-row justify-center items-center h-24 bg-gray-100">
+      asdf
+    </div>
+  );
+}
+
+export default NavigationBar;
