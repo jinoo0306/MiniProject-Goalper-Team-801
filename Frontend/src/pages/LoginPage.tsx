55d496378451e5c57c060e306a5f8afff321cbd4
j._.jinoo <jinoo0306@naver.com>
2024-02-19 20:02:04 +0900

    design : 로그인 화면 div틀 디자인

--- Frontend/src/pages/LoginPage.tsx
+++ Frontend/src/pages/LoginPage.tsx
@@ -1,8 +1,21 @@
 import React from "react";
+import NavigationBar from "../components/public/NavigationBar";
 import SignFormInputBasic from "../components/public/SignFormInputBasic";
+import SignFormAnimation from "../components/public/SignFormAnimation";
 
 function LoginPage() {
-  return <div>갑좌~</div>;
+  return (
+    <div className="flex flex-col w-screen h-screen">
+      <NavigationBar />
+      <div className="flex flex-row justify-center h-screen w-screen p-4">
+        <div className="flex flex-col justify-center h-11/12 w-1/2 items-center"></div>
+        <div className="border-l border-gray-100 h-11/12"></div>
+        <div className="flex flex-col justify-center h-11/12 w-1/2 items-center">
+          <SignFormAnimation />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LoginPage;
