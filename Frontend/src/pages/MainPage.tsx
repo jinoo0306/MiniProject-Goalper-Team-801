4fcba6603dd731129899d25af47244a491927cac
j._.jinoo <jinoo0306@naver.com>
2024-02-22 03:56:32 +0900

    design : title 수정과 로고 수정

--- Frontend/src/pages/MainPage.tsx
+++ Frontend/src/pages/MainPage.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import NavigationBar from "../components/public/NavigationBar";
 
 function MainPage() {
-  return <div>main페이지 입니다. 화이팅 해보자구!</div>;
+  return (
+    <div className="flex flex-col w-screen h-screen">
+      <NavigationBar />
+      <div className="flex flex-row justify-center items-center h-screen w-screen p-4">
+        <div className="flex flex-col h-full w-1/2 justify-center items-center p-16">
+          <div className="flex flex-col w-full h-full justify-center items-center rounded-3xl bg-gray-100">
+            <a className="text-6xl font-bmjua">Today's To Do List</a>
+          </div>
+        </div>
+
+        <div className="flex flex-col justify-center h-full w-1/2 items-center bg-blue-100"></div>
+      </div>
+    </div>
+  );
 }
 
 export default MainPage;
