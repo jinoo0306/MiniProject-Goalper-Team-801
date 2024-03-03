5027c45ca2f1603a10cf109c987e6c877e45d47a
j._.jinoo <jinoo0306@naver.com>
2024-03-03 20:27:01 +0900

    design : To Do List 페이지 퍼블리싱 완료

--- Frontend/src/pages/ToDoListPage.tsx
+++ Frontend/src/pages/ToDoListPage.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import NavigationBar from "../components/public/NavigationBar";
+import ToDoStatus from "../components/public/ToDoStatus";
 import CRUDButton from "../components/ToDoListPage/CRUDButton";
 import DateMove from "../assets/images/ToDoListPage/DateMove.png";
+import Empty from "../assets/images/ToDoListPage/ToDoListPageEmpty.png";
 
 function ToDoListPage() {
   const [currentDate, setCurrentDate] = useState<Date>(new Date());
@@ -23,21 +25,13 @@ function ToDoListPage() {
 
   return (
     <div className="flex flex-col w-screen h-screen">
-      {" "}
-      {/* 전체 화면을 차지하는 컨테이너 */}
       <NavigationBar />
       <div className="flex flex-row justify-center items-center h-screen w-screen p-4">
-        {" "}
-        {/* 네비게이션바를 제외한 전체 */}
-        <div className="flex flex-col h-full w-1/2 items-center p-4 bg-yellow-100">
-          {" "}
-          {/* 왼쪽 컨테이너 */}
+        <div className="flex flex-col h-full w-1/2 items-center p-4">
           <div className="flex pr-32 w-full justify-end">
             <CRUDButton colorCode={0} placeHolder="Add Task" />
           </div>
           <div className="flex flex-row justify-center items-center mt-8 text-6xl font-bmjua">
-            {" "}
-            {/* 날짜 */}
             <img
               src={DateMove}
               alt="DateMoveLeft"
@@ -52,17 +46,88 @@ function ToDoListPage() {
               onClick={() => changeDate(1)} // 오른쪽 버튼을 눌렀을 때 날짜를 하루 더하기
             />
           </div>
-          <div className="flex flex-row items-center w-3/4 h-32 bg-red-100 text-2xl mt-8 pl-8">
-            asdf
+          <div className="flex flex-row items-center w-3/4 h-24 text-2xl mt-8 pl-8 pr-12 font-extrabold">
+            <a className="w-full">To Do Title</a>
+            <a className="text-right">Status</a>
           </div>
           <hr className="border-t-2 w-3/4 border-gray-400" />
-          <div className="To Do List 컨테이너 flex flex-col items-center mt-8 w-full h-5/6 overflow-y-auto hideScrollBar">
-            asd
+          <div className="flex flex-col items-center w-full h-5/6 overflow-scrol hideScrollBar">
+            <hr className="border-t-2 w-3/4 border-gray-400" />
+            <div className="flex flex-row items-center text-gray-500 w-3/4 h-32 text-2xl pl-8 pr-6">
+              <a className="w-full">React 공부</a>
+              <ToDoStatus statusCode={2} />
+            </div>
+
+            <hr className="border-t-2 w-3/4 border-gray-400" />
+            <div className="flex flex-row items-center text-gray-500 w-3/4 h-32 text-2xl pl-8 pr-6">
+              <a className="w-full">수강신청</a>
+              <ToDoStatus statusCode={2} />
+            </div>
+
+            <hr className="border-t-2 w-3/4 border-gray-400" />
+            <div className="flex flex-row items-center text-gray-500 w-3/4 h-32 text-2xl pl-8 pr-6">
+              <a className="w-full">Next.js 공부</a>
+              <ToDoStatus statusCode={1} />
+            </div>
+
+            <hr className="border-t-2 w-3/4 border-gray-400" />
+            <div className="flex flex-row items-center text-gray-500 w-3/4 h-32 text-2xl pl-8 pr-6">
+              <a className="w-full">오늘도 사랑스럽개 정주행</a>
+              <ToDoStatus statusCode={0} />
+            </div>
+
+            <hr className="border-t-2 w-3/4 border-gray-400" />
+            <div className="flex flex-row items-center text-gray-500 w-3/4 h-32 text-2xl pl-8 pr-6">
+              <a className="w-full">오늘도 사랑스럽개 정주행</a>
+              <ToDoStatus statusCode={0} />
+            </div>
+
+            <hr className="border-t-2 w-3/4 border-gray-400" />
+            <div className="flex flex-row items-center text-gray-500 w-3/4 h-32 text-2xl pl-8 pr-6">
+              <a className="w-full">오늘도 사랑스럽개 정주행</a>
+              <ToDoStatus statusCode={0} />
+            </div>
           </div>
         </div>
-        <div className="flex flex-col h-full w-1/2 bg-blue-100">
-          {" "}
-          {/* 오른쪽 컨테이너 */}
+        <div className="border-l border-gray-100 w-px h-full"></div>
+        <div className="flex flex-col h-full w-1/2">
+          {false && (
+            <div className="flex flex-col justify-center items-center w-full h-full text-2xl font-bold">
+              <img src={Empty} alt="Empty" className="w-96" />
+              Click on To Do TItle to see details.
+            </div>
+          )}
+          {true && (
+            <div className="flex flex-col items-center w-full h-full p-4">
+              <div className="flex pr-32 w-full justify-end">
+                <CRUDButton colorCode={1} placeHolder="Delete Task" />
+                <div className="w-4"></div>
+                <CRUDButton colorCode={2} placeHolder="Save Task" />
+              </div>
+              <a className="text-2xl w-3/4 font-bold mt-12 m-4 pl-4">
+                To Do Title
+              </a>
+              <input
+                type="text"
+                className="w-3/4 h-20 p-4 border-2 border-black rounded-2xl"
+                placeholder="Type your task here..."
+              ></input>
+
+              <a className="text-2xl w-3/4 font-bold mt-12 m-4 pl-4">Status</a>
+              <select className="w-3/4 h-20 p-4 border-2 border-black rounded-2xl text-gray-600">
+                <option value="0">Not Started</option>
+                <option value="1">In Progress</option>
+                <option value="2">Completed</option>
+              </select>
+              <a className="text-2xl w-3/4 font-bold mt-12 m-4 pl-4">
+                Detailed
+              </a>
+              <textarea
+                className="w-3/4 h-96 p-4 border-2 border-black rounded-2xl"
+                placeholder="Type your detailed task here..."
+              ></textarea>
+            </div>
+          )}
         </div>
       </div>
     </div>
