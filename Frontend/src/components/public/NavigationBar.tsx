c8ccd61e02f832e9e0c7b8198d6af2fcdd48bf86
j._.jinoo <jinoo0306@naver.com>
2024-03-04 22:51:18 +0900

    design : 링크 이동 수정

--- Frontend/src/components/public/NavigationBar.tsx
+++ Frontend/src/components/public/NavigationBar.tsx
@@ -22,7 +22,12 @@ function NavigationBar() {
   return (
     <div className="flex flex-row justify-center items-center h-24 bg-gray-100">
       <div className="flex flex-row p-6 w-4/5">
-        <a className="text-4xl font-passion mr-24">Goalper</a>
+        <a
+          className="text-4xl font-passion mr-24 cursor-pointer"
+          onClick={() => navigate("/main")}
+        >
+          Goalper
+        </a>
         <a
           className="text-2xl ml-10 cursor-pointer mr-20"
           onClick={() => navigate("/todolist")}
--- Frontend/src/pages/BulletInPage.tsx
+++ Frontend/src/pages/BulletInPage.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import NavigationBar from "../components/public/NavigationBar";
+import CRUDButton from "../components/ToDoListPage/CRUDButton";
 
 function BulletInPage() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col w-screen h-screen">
+      <NavigationBar />
+      <div className="flex flex-col items-center h-screen w-screen p-4">
+        <div className="flex mt-8 mb-8 w-3/4 justify-end">
+          <CRUDButton colorCode={3} placeHolder="Write a post" />
+        </div>
+        <div className="flex flex-col justify-center items-center w-full h-24 bg-white">
+          <div className="flex flex-row w-full h-full justify-center items-center">
+            asdf
+          </div>
+          <hr className="w-3/4 border-1 border-gray-300" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default BulletInPage;
